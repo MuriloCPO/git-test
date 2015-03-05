99aa6f163a19ebb2a41c53a913f9fab59c223644
41424980 <41424980@090011-P33S13.macklabs.br>
2015-03-05 11:34:50 -0300

    done

---
+++ ex2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>DESC</th>
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr bgcolor="black">
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr bgcolor="black">
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr bgcolor="black">
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+            <tr>
+                <td>id</td>
+                <td>nome</td>
+                <td>desc</td>
+            </tr>
+        </table>
+    </body>
+</html>
